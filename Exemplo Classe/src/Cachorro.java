7257ef49c86f5da56de278935af210194834dafc
Orlewilson Maia <orlewilson.maia@gmail.com>
2019-09-30 22:21:29 -0400

    atualizacao do projeto com exemplo de encapsulamento

--- Exemplo Classe/src/Cachorro.java
+++ Exemplo Classe/src/Cachorro.java
@@ -3,44 +3,108 @@
  * Prof.:		Orlewilson B. Maia
  * Autor:		Seu nome
  * Data:		30/08/2019
+ * Atualização:	27/09/2019
  * Descrição:	Classe para representar um
- * 				cachorro
+ * 				cachorro sem encapsulamento e com encapsulamento
  * 
  * */
 
 public class Cachorro {
 
-	// atributos
-	String raca;
-	String cor;
-	float comprimento;
-	float peso;
-	String apelido;
+	// atributos (sem encapsulamento)
+//	String raca;
+//	String cor;
+//	float comprimento;
+//	float peso;
+//	String apelido;
 	
+	// atributos (com encapsulamento)
+	private String raca;
+	private String cor;
+	private float comprimento;
+	private float peso;
+	private String apelido;
 
 	// métodos
+	// alterar conteúdo do atributo raca
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	// obter conteúdo do atributo raca
+	public String getRaca() {
+		return this.raca;
+	}
+
+	// alterar conteúdo do atributo cor
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	// obter conteúdo do atributo cor
+	public String getCor() {
+		return this.cor;
+	}
+
+	// alterar conteúdo do atributo apelido
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	// obter conteúdo do atributo apelido
+	public String getApelido() {
+		return this.apelido;
+	}
+
+	// alterar conteúdo do atributo comprimento
+	public void setComprimento(float comprimento) {
+
+		// verifiicar se comprimento é > 0
+		if (comprimento > 0) {
+			this.comprimento = comprimento;
+		} else {
+			System.out.println("informe um valor positivo");
+		}
+	}
+
+	// obter conteúdo do atributo comprimento
+	public float getComprimento() {
+		return this.comprimento;
+	}
+
+	// alterar conteúdo do atributo peso
+	public void setPeso(float peso) {
+
+		if (peso > 0) {
+			this.peso = peso;
+		} else {
+			System.out.println("informe um valor positivo");
+		}
+	}
+
+	// obter conteúdo do atributo peso
+	public float getPeso() {
+		return this.peso;
+	}
+
 	void latir() {
 		System.out.println("Au, au!!!!");
 	}
-	
+
 	void correr() {
 		System.out.println("correndo...");
 	}
-	
+
 	void morder() {
 		System.out.println("mordendo...");
-	}	
+	}
+
+	// mostrar informações dos atributos
+	public void mostrarInformacoes() {
+		System.out.println("Apelido: " + getApelido());
+		System.out.println("Raça: " + getRaca());
+		System.out.println("Cor: " + getCor());
+		System.out.println("Peso: " + getPeso());
+		System.out.println("Comprimento: " + getComprimento());
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Exemplo Classe/src/Main.java
+++ Exemplo Classe/src/Main.java
@@ -3,7 +3,8 @@
  * Prof.:		Orlewilson B. Maia
  * Autor:		Seu nome
  * Data:		30/08/2019
- * Descrição:	Exemplo Classe
+ * Atualização:	27/09/2019
+ * Descrição:	Exemplo Classe sem e com encapsulamento
  * 
  * */
 
@@ -14,45 +15,61 @@ public class Main {
 		// criar objetos
 		Cachorro toto = new Cachorro();
 		
-		// alterando conteúdo dos atributos
-		toto.apelido = "toto";
-		toto.comprimento = 1.2f;
-		toto.cor = "amarelo";
-		toto.peso = 20f;
-		toto.raca = "pinch";
-	
-		// mostrar conteúdos dos atributos
-		System.out.println("Apelido: " + 
-		toto.apelido);
+		// alterando conteúdo dos atributos (sem encapsulamnto)
+//		toto.apelido = "toto";
+//		toto.comprimento = 1.2f;
+//		toto.cor = "amarelo";
+//		toto.peso = 20f;
+//		toto.raca = "pinch";
 		
-		System.out.println("Comprimento: " + 
-				toto.comprimento);
+		// alterando conteúdo dos atributos (com encapsulamento)
+		toto.setApelido("toto");
+		toto.setComprimento(1.2f);
+		toto.setCor("amarelo");
+		toto.setPeso(20f);
+		toto.setRaca("pinch");
+			
 		
-		System.out.println("Cor: " + 
-				toto.cor);
+//		// mostrar conteúdos dos atributos (sem encapsulamnto)
+//		System.out.println("Apelido: " + 
+//		toto.apelido);
+//		
+//		System.out.println("Comprimento: " + 
+//				toto.comprimento);
+//		
+//		System.out.println("Cor: " + 
+//				toto.cor);
+//		
+//		System.out.println("Peso: " + 
+//				toto.peso);
+//		
+//		System.out.println("Raça: " + 
+//				toto.raca);
+
+		// mostrar conteúdos dos atributos (com encapsulamnto)
+//		System.out.println("Apelido: " + 
+//		toto.getApelido());
+//		
+//		System.out.println("Comprimento: " + 
+//				toto.getComprimento());
+//		
+//		System.out.println("Cor: " + 
+//				toto.getCor());
+//		
+//		System.out.println("Peso: " + 
+//				toto.getPeso());
+//		
+//		System.out.println("Raça: " + 
+//				toto.getRaca());
 		
-		System.out.println("Peso: " + 
-				toto.peso);
-		
-		System.out.println("Raça: " + 
-				toto.raca);
-		
+		// mostrar dados do cachorro
+		toto.mostrarInformacoes();
+
 		// chamando os métodos
 		toto.correr();
 		
 		toto.morder();
 		
 		toto.latir();
-		
-		
 	}
 }
-
-
-
-
-
-
-
-
-
